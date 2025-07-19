de0e57372c46328b3aaf582393109a8cea83dd72
Giorgos Vervelakis <gverv@hotmail.com>
2025-07-19 22:03:47 +0300

    mysql localhost

--- synetairismos/settings.py
+++ synetairismos/settings.py
@@ -81,10 +81,23 @@ WSGI_APPLICATION = 'synetairismos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gverv$synet',
+        'USER': 'gverv$synet',
+        'PASSWORD': 'pefkos@1932',  # Βάλε εδώ το password σου
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
